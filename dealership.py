bac739b25df5ca2f19448eb88540802febd5c5c8
Luke Yamaguchi <lukeyamaguchi@Lukes-MacBook-Air.local>
2024-07-17 20:50:50 -0700

    sell car function created, still work in progress

--- dealership.py
+++ dealership.py
@@ -8,4 +8,13 @@ class Dealership():
     
     def get_revenue(self):
         return self.revenue
-   
+    
+    def sell_car(self, modelN, used, employee_who_sold):
+        # find car
+        for i in self.inventory:
+            if i.model == modelN:
+                break
+        
+        self.inventory.remove(i)
+
+        employee_who_sold.increment_cars_sold()
